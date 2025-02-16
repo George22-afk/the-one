742552451adccbeafb78a61da2ddbb476c15a544
David Korshak <dkorshak@my.claridenschool.org>
2025-02-16 22:58:14 +0000

    Updated data on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "David Korshak",
+  title: "Overall Fun Person",
+  email: "davidj@fastmail.us",
+  gitHub: "George22-afk",
+  instagram: "N/A",
+  linkedIn: "N/A",
+  medium: "N/A",
+  twitter: "N/A",
+  youTube: "N/A",
 };
 
 const primaryColor = "#4E567E";
